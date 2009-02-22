e26f767ecdef1e7036513451f6f18e66a0b26f84
Michael Pavlovsky <ilq00294@ILQHFAATC1WS142.code1.emi.philips.com>
2009-02-22 23:37:13 +0200

    prebuild script is completed

--- utils/pack_all.zsh
+++ utils/pack_all.zsh
@@ -1,3 +1,21 @@
+#!/bin/zsh
 target="prebuild.tar.gz"
-tar -zcvf $target ../prebuild&&scp $target pavlov@shell.berlious.de:\~
-
+server='shell.berlios.de:~'
+function Clean()
+{
+    echo "removing the $target"
+    rm -rf $target
+}
+function Archive()
+{
+   echo "creating the archive" 
+   tar -zcvf $target ../prebuild
+   return $?     
+}
+function Upload()
+{
+    echo "uploading $target file to $server"
+    scp $target $server
+}
+Archive&&Upload
+Clean
